287908f0bbb49918b6e0d0fdaebd533017fceb63
alexcp99 <36475569+alexcp99@users.noreply.github.com>
2018-10-22 17:18:57 +0200

    Create altura.ino

---
+++ altura.ino
@@ -0,0 +1,52 @@
+
+const int EchoPin = 16;
+const int TriggerPin = 17;
+
+void setup() {
+  
+Serial.begin(115200);
+
+pinMode(TriggerPin, OUTPUT);
+
+pinMode(EchoPin, INPUT);
+}
+
+void loop() {
+  
+Serial.print("Distancia: ");
+
+Serial.println(distancia(TriggerPin, EchoPin));
+
+delay(1000);
+
+}
+
+
+int distancia(int TriggerPin, int EchoPin) {
+  
+long duracion, distanciaCm;
+
+digitalWrite(TriggerPin, LOW); 
+
+//nos aseguramos señal baja al principio
+
+delayMicroseconds(4);
+
+digitalWrite(TriggerPin, HIGH); 
+
+//generamos pulso de 10us
+
+delayMicroseconds(10);
+
+digitalWrite(TriggerPin, LOW);
+
+duracion = pulseIn(EchoPin, HIGH); 
+ 
+//medimos el tiempo del pulso
+
+distanciaCm = duracion * 10 / 292 / 2;  
+ 
+//convertimos a distancia
+
+return distanciaCm;
+}
